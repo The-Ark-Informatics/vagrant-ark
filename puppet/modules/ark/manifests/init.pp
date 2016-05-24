2286f8e0020f2c4d54990c288d752c60639fef4c
George Gooden <gecgooden@gmail.com>
2016-05-24 13:27:11 +0800

    Changed mvn install to mvn package to not invoke the integration tests

--- puppet/modules/ark/manifests/init.pp
+++ puppet/modules/ark/manifests/init.pp
@@ -25,7 +25,7 @@ class ark {
 	}->
 	exec { 'buildArk':
 		cwd => '/home/vagrant/ark/',
-		command => '/usr/bin/mvn install -T8',
+		command => '/usr/bin/mvn package -T8',
 		timeout => 0,
 		require => Package['maven'],
 	}->
